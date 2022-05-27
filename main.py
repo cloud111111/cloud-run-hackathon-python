13b73a12d6cb4e511f9c44fbc22585b1eacf7a02
cloud111111 <106362083+cloud111111@users.noreply.github.com>
2022-05-27 11:44:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-# moves = ['R', 'R', 'R', 'R', 'R', 'R', 'R']
+# moves = ['T', 'T',  'L', 'T',  'L',  'L', 'T']
 moves = ['F', 'T', 'L', 'R']
 
 @app.route("/", methods=['GET'])
