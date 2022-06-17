f63d60c5d49ceb8712d844eda3be5e53e8b3f430
Team N <106362083+cloud111111@users.noreply.github.com>
2022-06-17 15:39:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-moves = ['L','F']
-# moves = ['F', 'F', 'T', 'T', 'L', 'R']
+# moves = ['L','F']
+moves = ['F', 'F', 'T', 'T', 'L', 'R']
 
 @app.route("/", methods=['GET'])
 def index():
